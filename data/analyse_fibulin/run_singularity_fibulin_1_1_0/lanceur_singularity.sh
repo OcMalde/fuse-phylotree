374992b50661e8e7080d12314beac7c904db09b2
Elisa Chenel <echenel@ptb-5cg43445vx.irisa.fr>
2025-07-23 08:58:46 +0200

    Add run_singularity_fibulin_1_1_0 with his files

---
+++ data/analyse_fibulin/run_singularity_fibulin_1_1_0/lanceur_singularity.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=lanceur
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=50G
+#SBATCH --output=sortie_lanceur.out
+
+date
+
+# File paths
+image_path="fuse_phylotree.sif"
+file_fasta="fibulin_59.fasta"
+file_annotation="ppi_shared.csv"
+gene_tree="fibulin_tree_root.tree"
+output_dir="dir_analysis_fuse_phylotree"
+
+# Command to launch tool
+cmd="python3 /fuse-phylotree/fuse-phylotree.py --output_directory ${output_dir} $file_fasta $file_annotation $gene_tree"
+
+# Start analysis
+singularity exec ${image_path} ${cmd}
+
+
+date
